f838797d2a573ea2d249f826f8d761d4745b0179
wojder <wojdera@demioorg.pl>
2014-10-23 17:42:10 +0200

    main.xml changes

---
+++ src/com/example/ModernArtUI/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ModernArtUI;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.test_menu, menu);
+        return true;
+    }
+}
