0c65230f6bee3d144739202edd019e1cd7c8bbc3
wojder <wojdera@demioorg.pl>
2014-10-29 18:47:11 +0100

    final version of app, edited dialog, added new style

--- src/com/example/ModernArtUI/MainActivity.java
+++ src/com/example/ModernArtUI/MainActivity.java
@@ -4,11 +4,10 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.*;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /**
@@ -33,9 +32,11 @@ public class MainActivity extends Activity {
 
             case R.id.action_settings2:
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.AlertDialogCustom));
                 LayoutInflater inflater = this.getLayoutInflater();
-                builder.setMessage(R.string.builder_message).setPositiveButton(R.string.positive, new DialogInterface.OnClickListener() {
+                builder
+//                        .setMessage(R.string.builder_message)
+                          .setPositiveButton(R.string.positive, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
@@ -45,9 +46,17 @@ public class MainActivity extends Activity {
                      @Override
                      public void onClick(DialogInterface dialog, int which) {
                      }
-                }).setTitle(R.string.builder_title).setView(inflater.inflate(R.layout.webview, null));
+                })
+                  .setView(inflater.inflate(R.layout.dialog_view, null));
                 AlertDialog alert = builder.create();
                 alert.show();
+
+                Button buttonNeg = alert.getButton(DialogInterface.BUTTON_NEGATIVE);
+                buttonNeg.setBackgroundColor(Color.parseColor("#A41900"));
+
+                Button buttonPos = alert.getButton(DialogInterface.BUTTON_POSITIVE);
+                buttonPos.setBackgroundColor(Color.parseColor("#A41900"));
+
                 return true;
 
             default:
